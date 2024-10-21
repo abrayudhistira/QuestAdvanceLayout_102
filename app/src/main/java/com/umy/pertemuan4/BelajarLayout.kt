1b5aed5ec1413938b88abbcf84a50e80ee11157a
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-10-21 17:42:24 +0700

    Final App , Quest Advance Layout

---
+++ app/src/main/java/com/umy/pertemuan4/BelajarLayout.kt
@@ -0,0 +1,125 @@
+package com.umy.pertemuan4
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Preview(showBackground = true)
+
+@Composable
+fun BelajarLayout (
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ){
+        HeaderSection()
+        DetailMhs(
+            param = "Nama",
+            argu = "Abra Yudhistira Rachmadi"
+        )
+        DetailMhs(
+            param = "NIM",
+            argu = "20220140102"
+        )
+        DetailMhs(
+            param = "Prodi",
+            argu = "Teknologi Informasi"
+        )
+        DetailMhs(
+            param = "Fakultas",
+            argu = "Teknik"
+        )
+        DetailMhs(
+            param = "Universitas",
+            argu = "Universitas Muhammadiyah Yogyakarta"
+        )
+        DetailMhs(
+            param = "Alamat",
+            argu = "St. Rakai Layang 150 Kasihan, Bantul, Yogyakarta"
+        )
+        DetailMhs(
+            param = "Email",
+            argu = "admin@gmail.com"
+        )
+    }
+}
+
+@Composable
+fun HeaderSection(){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ){
+            Box(
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                Image(painter = painterResource(id = R.drawable.res),
+                    contentDescription = "ini adalah icon",
+                    modifier = Modifier.clip(CircleShape)
+                )
+                Icon(
+                    Icons.Filled.CheckCircle,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+            Column (
+                modifier = Modifier.padding(14.dp)
+            ) {
+                Text(text = "Umbrella Corporation")
+                Text(text = "For The Brave Soldiers")
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailMhs(
+    param: String, argu: String
+) {
+    Column (
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = param,
+                modifier = Modifier.weight(0.8f))
+            Text(text = ":",
+                modifier = Modifier.weight(0.2f))
+            Text(text = argu,
+                modifier = Modifier.weight(2f))
+        }
+    }
+}
